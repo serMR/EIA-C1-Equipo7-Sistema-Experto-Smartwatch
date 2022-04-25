e782a47d33df07b4716d4fb0d93915ab4a6ac7c6
Sergio Rodriguez <rodriguezmsergio@gmail.com>
2022-04-24 22:37:35 -0300

    Update tests

--- src/main/test/SmartwatchTestCases.java
+++ src/main/test/SmartwatchTestCases.java
@@ -1,8 +1,4 @@
 
-//public class SmartwatchTestCases {
-//}
-
-
 import static utils.TestCaseUtils.assertResults;
 import static utils.TestCaseUtils.print;
 
@@ -61,7 +57,7 @@ public class SmartwatchTestCases {
 		print("----------End----------");
 		print();
 	}
-
+	
 	@Test
 	public void cargarBateriaTest() {
 		
@@ -87,5 +83,202 @@ public class SmartwatchTestCases {
 		AccionRecomendada valorEsperado = AccionRecomendada.CargarBateria;
 		assertResults(recomendacion, valorEsperado);
 	}
+	
+	@Test
+	public void cambiarCargador() {
+		
+		print("Caso de prueba: Cambiar Cargador");
+		
+		Bateria bateria = new Bateria();
+		bateria.setCapacidadPorcentual(95);
+		PinCarga pinCarga = new PinCarga();
+		pinCarga.setDiferencialPotencialPin(4.9);
+		CargadorSmartwatch cargador = new CargadorSmartwatch();
+		cargador.setDiferencialPotencial(2.5);
+		SistemaOperativo sistemaOp = new SistemaOperativo();
+		ModuloBluetooth bluetooth = new ModuloBluetooth();
+		Pantalla pantalla = new Pantalla();
+		
+		Smartwatch smartwatch = new Smartwatch(bateria, pinCarga, pantalla, sistemaOp, bluetooth, cargador);
+		
+		print(smartwatch);
+		sessionStatefull.insert(smartwatch);
+		sessionStatefull.fireAllRules();
+		
+		Recomendacion recomendacion = smartwatch.getRecomendacion();
+		AccionRecomendada valorEsperado = AccionRecomendada.CambiarCargador;
+		assertResults(recomendacion, valorEsperado);
+	}
+	
+	@Test
+	public void cambiarPinDeCarga() {
+		
+		print("Caso de prueba: Cambiar Pin de carga");
+		
+		Bateria bateria = new Bateria();
+		bateria.setCapacidadPorcentual(95);
+		PinCarga pinCarga = new PinCarga();
+		pinCarga.setDiferencialPotencialPin(2.2);
+		CargadorSmartwatch cargador = new CargadorSmartwatch();
+		cargador.setDiferencialPotencial(4.9);
+		SistemaOperativo sistemaOp = new SistemaOperativo();
+		ModuloBluetooth bluetooth = new ModuloBluetooth();
+		Pantalla pantalla = new Pantalla();
+		
+		Smartwatch smartwatch = new Smartwatch(bateria, pinCarga, pantalla, sistemaOp, bluetooth, cargador);
+		
+		print(smartwatch);
+		sessionStatefull.insert(smartwatch);
+		sessionStatefull.fireAllRules();
+		
+		Recomendacion recomendacion = smartwatch.getRecomendacion();
+		AccionRecomendada valorEsperado = AccionRecomendada.CambiarPinCarga;
+		assertResults(recomendacion, valorEsperado);
+	}
+	
+	@Test
+	public void resetSOPorBootError() {
+		
+		print("Caso de prueba: Resetear el SO a fabrica por error de booteo");
+		
+		Bateria bateria = new Bateria();
+		bateria.setCapacidadPorcentual(95);
+		PinCarga pinCarga = new PinCarga();
+		CargadorSmartwatch cargador = new CargadorSmartwatch();
+		SistemaOperativo sistemaOp = new SistemaOperativo();
+		sistemaOp.setInterfaz(SOInterfaz.SinImagen);
+		sistemaOp.setVelocidadRespuesta(SOVelocidadRespuesta.Normal);
+		sistemaOp.setArranque(SOArranque.Normal);
+		ModuloBluetooth bluetooth = new ModuloBluetooth();
+		Pantalla pantalla = new Pantalla();
+		
+		Smartwatch smartwatch = new Smartwatch(bateria, pinCarga, pantalla, sistemaOp, bluetooth, cargador);
+		
+		print(smartwatch);
+		sessionStatefull.insert(smartwatch);
+		sessionStatefull.fireAllRules();
+		
+		Recomendacion recomendacion = smartwatch.getRecomendacion();
+		AccionRecomendada valorEsperado = AccionRecomendada.ResetearSOFabrica;
+		assertResults(recomendacion, valorEsperado);
+	}
 
+	@Test
+	public void resetSOPorPerformanceError() {
+		
+		print("Caso de prueba: Resetear el SO a fabrica por saturacion de memoria");
+		
+		Bateria bateria = new Bateria();
+		bateria.setCapacidadPorcentual(95);
+		PinCarga pinCarga = new PinCarga();
+		CargadorSmartwatch cargador = new CargadorSmartwatch();
+		SistemaOperativo sistemaOp = new SistemaOperativo();
+		sistemaOp.setInterfaz(SOInterfaz.ImagenNormal);
+		sistemaOp.setVelocidadRespuesta(SOVelocidadRespuesta.Lenta);
+		sistemaOp.setArranque(SOArranque.Normal);
+		ModuloBluetooth bluetooth = new ModuloBluetooth();
+		Pantalla pantalla = new Pantalla();
+		
+		Smartwatch smartwatch = new Smartwatch(bateria, pinCarga, pantalla, sistemaOp, bluetooth, cargador);
+		
+		print(smartwatch);
+		sessionStatefull.insert(smartwatch);
+		sessionStatefull.fireAllRules();
+		
+		Recomendacion recomendacion = smartwatch.getRecomendacion();
+		AccionRecomendada valorEsperado = AccionRecomendada.ResetearSOFabrica;
+		assertResults(recomendacion, valorEsperado);
+	}
+
+	@Test
+	public void cambiarPantallaPorDanio() {
+		
+		print("Caso de prueba: Cambiar pantalla por daño");
+
+		Bateria bateria = new Bateria();
+		bateria.setCapacidadPorcentual(95);
+
+		PinCarga pinCarga = new PinCarga();
+		CargadorSmartwatch cargador = new CargadorSmartwatch();
+		ModuloBluetooth bluetooth = new ModuloBluetooth();
+
+		SistemaOperativo sistemaOp = new SistemaOperativo();
+		sistemaOp.setInterfaz(SOInterfaz.SinRespuesta);
+
+		Pantalla pantalla = new Pantalla();
+		pantalla.setTouchscreen(PantallaTouchscreen.NoDaniado);
+		pantalla.setEstado(PantallaEstado.Apagado);
+		pantalla.setPresentaGolpes(true);
+
+		Smartwatch smartwatch = new Smartwatch(bateria, pinCarga, pantalla, sistemaOp, bluetooth, cargador);
+
+		print(smartwatch);
+		sessionStatefull.insert(smartwatch);
+		sessionStatefull.fireAllRules();
+
+		Recomendacion recomendacion = smartwatch.getRecomendacion();
+		AccionRecomendada valorEsperado = AccionRecomendada.CambiarPantalla;
+		assertResults(recomendacion, valorEsperado);
+	}
+	
+	@Test
+	public void cambiarPantallaPorTouchScreen() {
+		
+		print("Caso de prueba: Cambiar pantalla por touchscreen dañado");
+
+		Bateria bateria = new Bateria();
+		bateria.setCapacidadPorcentual(95);
+
+		PinCarga pinCarga = new PinCarga();
+		CargadorSmartwatch cargador = new CargadorSmartwatch();
+		ModuloBluetooth bluetooth = new ModuloBluetooth();
+
+		SistemaOperativo sistemaOp = new SistemaOperativo();
+		sistemaOp.setInterfaz(SOInterfaz.ImagenNormal);
+
+		Pantalla pantalla = new Pantalla();
+		pantalla.setTouchscreen(PantallaTouchscreen.Daniado);
+		pantalla.setEstado(PantallaEstado.Prendida);
+		pantalla.setPresentaGolpes(false);
+
+		Smartwatch smartwatch = new Smartwatch(bateria, pinCarga, pantalla, sistemaOp, bluetooth, cargador);
+
+		print(smartwatch);
+		sessionStatefull.insert(smartwatch);
+		sessionStatefull.fireAllRules();
+
+		Recomendacion recomendacion = smartwatch.getRecomendacion();
+		AccionRecomendada valorEsperado = AccionRecomendada.CambiarPantalla;
+		assertResults(recomendacion, valorEsperado);
+	}
+	
+	@Test
+	public void cambiarModuloBluetooth() {
+		
+		print("Caso de prueba: Cambiar modulo bluetooth");
+
+		Bateria bateria = new Bateria();
+		bateria.setCapacidadPorcentual(95);
+
+		PinCarga pinCarga = new PinCarga();
+		Pantalla pantalla = new Pantalla();
+		CargadorSmartwatch cargador = new CargadorSmartwatch();
+		
+		ModuloBluetooth bluetooth = new ModuloBluetooth();
+		bluetooth.setPermiteEmparejamiento(false);
+
+		SistemaOperativo sistemaOp = new SistemaOperativo();
+		sistemaOp.setInterfaz(SOInterfaz.ImagenNormal);
+		sistemaOp.setVelocidadRespuesta(SOVelocidadRespuesta.Normal);
+
+		Smartwatch smartwatch = new Smartwatch(bateria, pinCarga, pantalla, sistemaOp, bluetooth, cargador);
+
+		print(smartwatch);
+		sessionStatefull.insert(smartwatch);
+		sessionStatefull.fireAllRules();
+
+		Recomendacion recomendacion = smartwatch.getRecomendacion();
+		AccionRecomendada valorEsperado = AccionRecomendada.ReempleazarModBluetooth;
+		assertResults(recomendacion, valorEsperado);
+	}
 }
